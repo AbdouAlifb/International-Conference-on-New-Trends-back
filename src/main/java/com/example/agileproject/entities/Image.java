7ead2360f6456b56ec80fff2c1533f378830a9c4
kaabouc <kaabouchmohamed13@gmail.com>
2023-12-25 23:33:16 +0000

    crud system de gallery

--- src/main/java/com/example/agileproject/entities/Image.java
+++ src/main/java/com/example/agileproject/entities/Image.java
@@ -1,2 +1,30 @@
-package com.example.agileproject.entities;public class Image {
+package com.example.agileproject.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "date")
+    private Date date;
+
+    @Lob
+    @Column(name = "image", columnDefinition = "BLOB")
+    private byte[] image;
+
+
+    @Column(name = "filename")
+    private String filename;
+
 }
